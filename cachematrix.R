a99512ec3835a1ac613b8c61adb93779e725d77d
AliAlarafat <Ali@insanitek.net>
2014-07-27 04:41:07 +0300

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+ ## This function computes the inverse of the special
+ ## "matrix" returned by `makeCacheMatrix`as variable x. If the inverse has
+ ##  already been calculated (and the matrix has not changed), then the
+ ## `cachesolve` should retrieve the inverse from the cache.
+ ##  After retrieving the inverse it will be assigned to variable m
+ ##  Variable m will not be returned but rather set to x then printed
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+  x <<- y
+  m <<- NULL
+}
 
+get <- function() x
+setmatrix <- function(solve) m<<- solve
+getmatrix <- function() m
+list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    matrix <- x$get()
+    m <- solve(matrix, ...)
+    x$setmatrix(m)
+    m
 }
+
+ ## I struggled a lot with this and used as much help I can from the instructor and fellow students
+ ## This is my best attempt at solving the assignment. Thank you.
